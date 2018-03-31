e3ca2cb13405ade82e297dcca859aaaadb8e0bc6
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 19:55:45 -0400

    Created .bin/www entry file and converted previous server file into middleware.

---
+++ .bin/www.js
@@ -0,0 +1,64 @@
+const dotenv = require('dotenv');
+const http = require('http');
+const path = require('path');
+const db = require('../config/db');
+const app = require('../app');
+const logger = require('../utils/logger');
+
+dotenv.config();
+
+const normalizePort = (val) => {
+  var port = parseInt( val, 10 );
+
+  if ( isNaN( port ) ) {
+    return val;
+  }
+
+  if ( port >= 0 ) {
+    return port;
+  }
+
+  return false;
+};
+
+const host = process.env.HOST || 'http://127.0.0.1';
+const port = normalizePort(process.env.PORT || 3000);
+
+const onListening = () => {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+
+  logger.info(`> Server listening at http://${host}:${port}.\n`)
+};
+
+const onError = (err) => {
+  if (err.syscall !== 'listen') {
+    throw err;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (err.code) {
+    case 'EACCES':
+      process.stderr.write(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      process.stderr.write(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw err;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('listening', onListening);
+server.on('error', onError);
+
+server.listen(port, host);
