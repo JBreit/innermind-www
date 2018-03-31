be103e1519042daa93fd5df73e5ee1a0a6ee7b11
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 18:08:30 -0400

    Edited app/index.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
-const config = require('./config');
+const db = require('./config/db');
 
 dotenv.config();
 
 console.log(`HOST: ${process.env.HOST}`);
 console.log(`PORT: ${process.env.PORT}`);
-console.log(config.db);
+console.log(db);
