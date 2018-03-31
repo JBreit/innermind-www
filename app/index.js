4e36260a8534b7ecc96e5600f7fd25735f602c0e
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 18:27:39 -0400

    Started HTTP Server. Added logger utility.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
 const dotenv = require('dotenv');
+const http = require('http');
+const path = require('path');
 const db = require('./config/db');
+const logger = require('../utils/logger');
 
 dotenv.config();
 
-console.log(`HOST: ${process.env.HOST}`);
-console.log(`PORT: ${process.env.PORT}`);
-console.log(db);
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.write('Test');
+  req.pipe(res);
+});
+
+server.listen(port, host, () => { logger.info(`Server listening at http://${host}:${port}.\n`); });
