c3373c9da90304bc24216437708281eed454d856
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 19:16:48 -0400

    Edited development dependencies list.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,33 @@
 module.exports = {
+  "name": "innermind-www",
+  "version": "0.0.1",
   "extends": "airbnb-base",
+  "env": {
+    "browser": true,
+    "node": true,
+    "mocha": true,
+    "jasmine": true,
+    "phantomjs": true,
+    "protractor": true,
+    "serviceworker": true
+  },
+  "globals": {
+    "document": true,
+    "window": true
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures": {
+        "jsx": true
+    }
+  },
   "plugins": [
-    "import"
+    "import",
+    "react"
   ],
+  "root": true,
   "rules": {
     "comma-dangle": ["error", "never"],
     "indent": ["error", 2, { "SwitchCase": 1 }],
