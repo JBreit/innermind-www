4164c8e08fc0ebbcc334cb46df9cda378bedbdb3
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 18:48:53 -0400

    Edited HTTP Server for listening and error events. Normalized HTTP port.

--- app/index.js
+++ app/index.js
@@ -6,12 +6,61 @@ const logger = require('../utils/logger');
 
 dotenv.config();
 
+const normalizePort = (val) => {
+  var port = parseInt( val, 10 );
+
+  if ( isNaN( port ) ) {
+    return val;
+  }
+
+  if ( port >= 0 ) {
+    return port;
+  }
+
+  return false;
+};
+
 const host = process.env.HOST;
-const port = process.env.PORT;
+const port = normalizePort(process.env.PORT);
+
+const onListening = () => {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+
+  logger.info(`> Server listening at http://${host}:${port}.\n`)
+};
+
+const onError = (err) => {
+  if (err.syscall !== 'listen') {
+    throw err;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  switch (err.code) {
+    case 'EACCES':
+      process.stderr.write(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      process.stderr.write(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw err;
+  }
+};
 
 const server = http.createServer((request, response) => {
   response.write('Test');
   request.pipe(response);
 });
 
-server.listen(port, host, () => { logger.info(`Server listening at http://${host}:${port}.\n`); });
+server.on('listening', onListening);
+server.on('error', onError);
+
+server.listen(port, host);
