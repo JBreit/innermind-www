c795ca35f801f6c9a572b0a91e196133841ff698
Jason Breitigan <jason.breitigan@gmail.com>
2018-03-31 21:08:10 -0400

    Edited test response.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 const app = (request, response) => {
   console.log(request.method, request.url);
 
-  response.write('Test');
+  response.write(`${request.method} `);
+  response.write(request.url);
   request.pipe(response);
 };
 
