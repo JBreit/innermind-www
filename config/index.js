5157c886a100f80a453d3f331f923faf8ca108c1
Jason Breitigan <jason.breitigan@gmail.com>
2018-04-01 07:25:41 -0400

    Edited configuration.

--- config/index.js
+++ config/index.js
@@ -1,3 +1,17 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+[
+  'HOST',
+  'NODE_ENV',
+  'PORT'
+].forEach((name) => {
+  if (!process.env[name]) {
+    throw new Error(`Environment variable ${name} is missing.`);
+  }
+});
+
 const config = {
   env: process.env.NODE_ENV || 'development',
   server: {
