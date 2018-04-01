f30fb3c19d2b7b9e769f13599155d3da16a7ecd3
Jason Breitigan <jason.breitigan@gmail.com>
2018-04-01 08:29:47 -0400

    Edited ./app/index.js middleware.

--- app/index.js
+++ app/index.js
@@ -2,26 +2,32 @@ const app = (request, response) => {
   const { headers, method, url } = request;
   let body = [];
 
-  request
-    .on('error', (error) => {
-      process.stderr.write(error.stack);
-    })
-    .on('data', (chunk) => {
-      body.push(chunk);
-    })
-    .on('end', () => {
-      body = Buffer.concat(body).toString();
-      response
-        .on('error', (error) => {
-          process.stderr.write(error);
-        });
+  if (request.method === 'POST' && request.url === '/echo') {
+    request.pipe(response);
+  } else {
 
-      response.writeHead(200, {'Content-Type': 'application/json'});
+    request.on('error', (error) => {
+      process.stderr.write(error.stack);
+    });
+    
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      response.on('error', (error) => {
+        process.stderr.write(error);
+      });
+
+      response.writeHead(200, { 'Content-Type': 'application/json' });
 
       const responseBody = { headers, method, url, body };
 
       response.end(JSON.stringify(responseBody, null, 2));
     });
+  }
 };
 
 module.exports = app;
